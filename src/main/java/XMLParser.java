d472f84b67f34b6749634f65777939e7f31bbd35
gregory <GregoryAxenov@yandex.ru>
2019-06-29 01:06:46 +0300

    [feat] add new test class with measuring time and method for simple reading from file in XMLParser

--- src/main/java/XMLParser.java
+++ src/main/java/XMLParser.java
@@ -1,3 +1,21 @@
-public class XMLParser {
+import java.io.*;
 
+public class XMLParser {
+    public void parse(String filename) {
+        try {
+            File file = new File(filename);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 }

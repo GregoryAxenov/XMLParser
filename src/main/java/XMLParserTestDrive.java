d683ce5ead640e2ea94522122c61f463bdfaa009
gregory <GregoryAxenov@yandex.ru>
2019-07-01 12:31:48 +0300

    [perf] fix constructor parameter

--- src/main/java/XMLParserTestDrive.java
+++ src/main/java/XMLParserTestDrive.java
@@ -1,7 +1,7 @@
 public class XMLParserTestDrive {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        XMLParser.parse("/home/gregory/Downloads/address.xml");
+        XMLParser.parse("/home/gregory/Downloads/file.xml");
         ApartmentLocationHandler.showRepeats(Root.apartmentLocations);
         ApartmentLocationHandler.showCitiesWithFloors(ApartmentLocationHandler.removeDuplicates(Root.apartmentLocations));
         long finishTime = System.currentTimeMillis();
